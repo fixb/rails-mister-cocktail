ab64dc2380ba35153a4f67b64b49935318556a4b
Francois Baud <fxbaud@gmail.com>
2020-01-25 19:07:59 +0000

    with some style

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -8,6 +8,10 @@ class DosesController < ApplicationController
     @dose = Dose.new
   end
 
+  def show
+    @dose = Dose.find(params[:id])
+  end
+
   def create
     @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.new(dose_params)
